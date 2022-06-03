fbcb85429058f571656082f59142ad6d06f705fe
Hadoukenz <roach199@mail.ru>
2022-06-03 06:20:29 +0300

    Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld fHW =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld sHW =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(fHW == sHW);
+
+        Cat catOne = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(catOne == catTwo);
+
     }
 }
